d815afc5c57894d47893742c5896377de6df2899
Stephane Cayron <stephane.cayron@gmail.com>
2026-03-15 12:31:55 +0100

    version

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def _lazy(module, func):
 # ---------------------------------------------------
 
 ADMIN_ID = 5251772
-VERSION = "v20260314.1605"
+VERSION = "v20260315.1231"
 
 st.set_page_config(
     page_title="Amicale Cyclo Escalquens",
